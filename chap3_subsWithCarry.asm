3f7291f3f929e02ef735621c89cabb411bd6e327
carlinhocr <carlos.rienzi@gmail.com>
2023-01-08 00:17:47 -0300

    chapter 3 starting substraction

--- chap3_subsWithCarry.asm
+++ chap3_subsWithCarry.asm
@@ -10,3 +10,16 @@ WATCH $0338
         BYTE    $0E, $08, $0A, $00, $9E, $20, $28,  $32, $33, $30, $34, $29, $00, $00, $00
 
 *=$900 
+        LDA #$08
+        CLC             ;clearing the carry in substraction will give us the wrong answer
+        SBC #$05        ;as the carry is zero iw wrongly substracts 8 -5 -1 = 2
+        STA $0334
+
+;lets do it right by setting the carry to 1
+
+        LDA #$08
+        SEC             ;setting the carry bit
+        SBC #$05        ;as the carry is one it does  8 -5 -0 = 3
+        STA $0335
+
+        RTS
