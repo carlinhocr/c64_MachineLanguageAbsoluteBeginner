16d5851f79620328236380df90f5d2bdef53828a
carlinhocr <carlos.rienzi@gmail.com>
2023-01-08 00:01:30 -0300

    chapter 3 done with addition

--- chap3_addWithCarry.asm
+++ chap3_addWithCarry.asm
@@ -2,6 +2,8 @@
 WATCH $0334
 WATCH $0335
 WATCH $0336
+WATCH $0337
+WATCH $0338
 
 *=$0801
 
@@ -29,9 +31,22 @@ WATCH $0336
 ;force a carry and see the result on the next sum
 
         LDA #$03        
-        SEC ; set carry flag             
-        ADC #$05         
+        SEC             ; set carry flag             
+        ADC #$05        ; adds the carry to the 03 and the 05 resulting in 09
         STA $0336
+
+;add two byte numbers 6C67 and 49B2
+
+        LDA #$67        ;enter the first low byte
+        CLC             ;clear the carry flag
+        ADC #$B2        ;add the second low byte to the first
+        STA $0337       ;store the result of the low byte addition
+        LDA #$6C        ;load the first high bytes   
+        ADC #$49        ;add the acummulator + the carry + the second high byte
+        STA $0338       ;store the high byte result
+        ;the final result is the concatenation of the content of memory
+        ;positions 0337 and 0338
+
         RTS        
 
 
