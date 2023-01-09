913a3be2c3a08241e09bf6471522b70e23fd1a67
carlinhocr <carlos.rienzi@gmail.com>
2023-01-09 12:45:33 -0300

    chapter 6 starting

---
+++ chap6_jump.asm
@@ -0,0 +1,24 @@
+; 10 SYS (2304)
+
+*=$0801
+
+        BYTE    $0E, $08, $0A, $00, $9E, $20, $28,  $32, $33, $30, $34, $29, $00, $00, $00
+
+*=$900 
+; create a sprite   
+
+        LDA #$01
+        STA $D015
+        STA $D027
+        STA $2040
+        LDA #$32
+        STA $D000
+        STA $D001
+
+;move the sprite
+        LDA $D000
+REPITO  ADC #$04
+        STA $D000
+        JMP REPITO ; jump to a label so as to not use the absolute memory address
+
+        RTS
