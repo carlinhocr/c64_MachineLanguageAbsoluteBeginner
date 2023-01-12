5a74bf168f503f65a57bd6ec3275f6790868430b
carlinhocr <carlos.rienzi@gmail.com>
2023-01-12 11:35:52 -0300

    chapter 7

---
+++ chap7_multiply_Xregister.asm
@@ -0,0 +1,19 @@
+; 10 SYS (2304)
+WATCH $0334
+
+*=$0801
+
+        BYTE    $0E, $08, $0A, $00, $9E, $20, $28,  $32, $33, $30, $34, $29, $00, $00, $00
+
+*=$900 
+
+; MULTIPLY 7 X 3 
+
+        LDX #$03
+        LDA #$00
+loop    CLC
+        ADC #$07
+        DEX 
+        BNE loop
+        STA $0334       ;save the result
+        RTS
