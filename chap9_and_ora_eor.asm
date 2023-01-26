7a1d5f92b3cd7d1068b387018e97fd812ab04cd6
carlinhocr <carlos.rienzi@gmail.com>
2023-01-26 12:14:57 -0300

    chapter 9

--- chap9_and_ora_eor.asm
+++ chap9_and_ora_eor.asm
@@ -1,14 +1,23 @@
-; 10 SYS (2304)
 
-*=$0801
+WATCH $0334
+WATCH $0335
+WATCH $0336
 
-        BYTE    $0E, $08, $0A, $00, $9E, $20, $28,  $32, $33, $30, $34, $29, $00, $00, $00
 *=$0900
 
 ;test the AND, ORA, EOR instructions
 
-        LDA #$CA        ;1100 1010 TO THE ACCUMULATOR
-        AND #$9F        ; AND 1100 1010
-                        ;     1001 1111
+        LDA #$CA        ;     1100 1010 TO THE ACCUMULATOR
+        AND #$9F        ; AND 1001 1111
                         ;----------------
-        STA $0334       ;     1000 1010
+        STA $0334       ;     1000 1010         8A
+
+        LDA #$A2        ;     1010 0010 TO THE ACCUMULATOR
+        ORA #$84        ; OR  1000 0100
+                        ;----------------
+        STA $0335       ;     1010 0110         A6
+
+        EOR $0334       ;     1010 0010 FROM THE ACCUMULATOR
+                        ;     1000 0110
+                        ;----------------
+        STA $0336       ;     0010 1100         2C
